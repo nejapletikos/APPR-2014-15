758ca4e3c87ac6d02f43f2169aa6cfec0cc2f6d9
Neja Pletikos <neja.pletikos@gmail.com>
2014-12-07 17:46:47 +0100

    Sprememba poročila.

--- Grafi/grafi.R
+++ Grafi/grafi.R
@@ -1,13 +1,13 @@
 # prvi graf
 
 skupajn <- c(22913, 22035,18904)
-pdf("skupajnesrece.pdf", paper = "a4")
+pdf("skupajnesrece.pdf")
 barplot(skupajn, main = "Število prometnih nesreč - skupaj", names.arg = c("2011","2012","2013"))
 dev.off()
 
 #drugi graf
 
 steviloud <- c(42105,39971,34481)
-pdf("steviloudelezencev.pdf", paper = "a4")
+pdf("steviloudelezencev.pdf")
 barplot(steviloud, main = "Število udeležencev - skupaj", names.arg = c("2011","2012","2013"))
 dev.off()
